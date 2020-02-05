407e48855e2727d9ede79b1b908ed0d5a18c2aaf
dgomeztechedge <daniel.gomez@techedgegroup.com>
2020-02-05 16:39:23 +0100

    First Edition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private httpClient: HttpClient) {}
   title = 'PortraitMedia';
+  images = [];
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    this.httpClient
+      .get('https://api.imgur.com/3/album/MiuEd16/images', {
+        headers: {
+          Authorization: 'Client-ID d4c0728985acdf7'
+        }
+      })
+      .subscribe((data: any) => {
+        this.images = data.data;
+      });
+  }
+
+  thumbnail(link: string): string {
+    const longitud = link.length;
+    return `${link.slice(0, longitud - 4)}b${link.slice(longitud - 4, longitud)}`;
+  }
 }
